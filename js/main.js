e2132b8ca46128a2d01ae3dd8f58274135cbc8cc
ivwu22 <ivwu22@gmail.com>
2021-01-30 00:36:55 -0500

    search movie, return name, year, plot, genre, actors, poster image

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const form = document.querySelector('form')
-const info = document.querySelector('p');
+const title = document.getElementById('title');
+const year = document.getElementById('releaseYear');
+const genre = document.getElementById('genre');
+const plot = document.getElementById('plot');
+const actors = document.getElementById("actors");
+const poster = document.getElementById('poster');
 
 let requestUrlTitle = "http://www.omdbapi.com/?apikey=b749840a&t=";
 let input = document.querySelector('#input')
@@ -11,20 +16,23 @@ form.addEventListener('submit', (evt) => {
 // document.addEventListener("DOMContentLoaded", () => {
     
 const getMovieData = ('submit', (e) => {
-        // e.preventDefault();
-        fetch(requestUrlTitle+input.value)
+        e.preventDefault();
+        fetch(requestUrlTitle+(input.value.toLowerCase()))
             .then((responseData) => {
                 return responseData.json();
             })
             .then((jsonData) => {
                 // console.log(jsonData)
-                info.innerHTML = `${jsonData.Title}, ${jsonData.Year} it is about ${jsonData.Plot}`;
-                // info.innerHTML = jsonData.Plot;
-                // info.innerHTML = jsonData.title;
-                // info.innerHTML = jsonData.title;
+                title.textContent = jsonData.Title;
+                year.innerHTML = `This movie was released in ${jsonData.Year}`;
+                genre.innerHTML = `Genre: ${jsonData.Genre}`;
+                plot.innerHTML = `Brief plot: ${jsonData.Plot}`;
+                actors.innerHTML = `Actors: ${jsonData.Actors}`;
+                poster.src = jsonData.Poster
             })
             .catch((error) => {
                 console.log("errorrrr:", error)
-            });
+            })
     })
+
 form.addEventListener('submit', getMovieData)
