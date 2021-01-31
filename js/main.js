0b4df7028b44e8397dec95b0c23ab7ea0063051a
ivwu22 <ivwu22@gmail.com>
2021-01-31 15:47:31 -0500

    no api key

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ const rating = document.getElementById('rating')
 
 let jsonReturns = null;
 
-let requestUrlTitle = "http://www.omdbapi.com/?apikey=b749840a&s=";
-let requestUrlImDbId = "http://www.omdbapi.com/?apikey=b749840a&i="
+let requestUrlTitle = "http://www.omdbapi.com/?apikey="+key.apikey + "a&s=";
+let requestUrlImDbId = "http://www.omdbapi.com/?apikey=" + key.apikey + "a&i="
 let input = document.querySelector('#input')
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
